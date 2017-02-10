faf509eb8eb346e7360fd68c05cd785a88326fc6
keyserfaty <keserfaty@gmail.com>
2017-02-10 19:25:05 -0300

    add hide alert funcionality to alert

--- src/modules/_common/Alert/index.js
+++ src/modules/_common/Alert/index.js
@@ -3,17 +3,46 @@ import './styles.css';
 
 import FontAwesome from 'react-fontawesome';
 
+class AlertContainer extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      display: true
+    };
+  }
+
+  handleDisplay () {
+    this.setState({
+      display: !this.state.display
+    });
+  }
+
+  render () {
+    return (
+      <Alert
+        {...this.props}
+        {...this.state}
+        handleDisplay={() => this.handleDisplay()}
+      />
+    );
+  }
+}
+
 const Alert = props => {
-  const { type = 'warning', text } = props;
+  const { type = 'warning', text, handleDisplay, display } = props;
   return (
-    <div className='alert-container'>
-      <div className={`alert alert-${type}`}>
-        <FontAwesome className='alert-info' name='info-circle' />
-        <FontAwesome className='alert-remove' name='times' />
-        {text}
-      </div>
-    </div>
+    <span>
+      { display && (
+        <div className='alert-container'>
+          <div className={`alert alert-${type}`}>
+            <FontAwesome className='alert-info' name='info-circle' />
+            <FontAwesome onClick={handleDisplay} className='alert-remove' name='times' />
+            {text}
+          </div>
+        </div>
+      )}
+    </span>
   );
 };
 
-export default Alert;
+export default AlertContainer;
