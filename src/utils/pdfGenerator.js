1625878e3d772ff7c13d46cfc0838ec5c5813c25
keyserfaty <keserfaty@gmail.com>
2017-02-12 12:43:18 -0300

    fix bug with pdf

--- src/utils/pdfGenerator.js
+++ src/utils/pdfGenerator.js
@@ -1,51 +1,64 @@
 import pdfMake from '../../node_modules/pdfmake/build/pdfmake.min.js';
 import './libs/vfs_fonts';
 
+import { pipe } from 'ramda';
+
+//* Helper functions
+const generateText = item => [{
+  text: `Contado: 
+         $ ${item.price}
+        `,
+  alignment: 'center',
+  fontSize: 18
+}, {
+  text: `3 cuotas de: $ ${item.priceCard}`,
+  alignment: 'center'
+}, {
+  text: 'TASA DE INTERES 10%',
+  fontSize: 9,
+  alignment: 'center'
+}, {
+  text: `
+      12 cuotas de: $ ${item.priceCardInterest}
+      con Ahora 12`,
+  alignment: 'center'
+}, {
+  text: 'TASA DE INTERES 20%',
+  fontSize: 9,
+  alignment: 'center'
+}];
+
+/**
+ * Generates an array of array every three elements
+ * @param list
+ */
+const generateArrayGroups = list => list
+  .reduce((res, elem, i) => {
+    if (i % 3 !== 0) {
+      res[Math.floor(i / 3)].push([elem]);
+    } else {
+      res.push([elem]);
+    }
+
+    return res;
+  }, []);
+
 //* Generates a PDF file
 const pdfGenerator = list => {
   let docDefinition = {
     content: [{
       table: {
-        widths: [150, 150, 150],
-        // TODO: need to add empty arrays if number is not a multiple of three
-        body: [[]]
+        widths: [150, 150, 150]
       }
     }]
   };
 
   const parseContent = items => items.reduce((res, item) => {
-    const elem = [{
-      text: `Contado: 
-         $ ${item.price}
-        `,
-      alignment: 'center',
-      fontSize: 18
-    }, {
-      text: `3 cuotas de: $ ${item.priceCard}`,
-      alignment: 'center'
-    }, {
-      text: 'TASA DE INTERES 10%',
-      fontSize: 9,
-      alignment: 'center'
-    }, {
-      text: `
-      12 cuotas de: $ ${item.priceCardInterest}
-      con Ahora 12`,
-      alignment: 'center'
-    }, {
-      text: 'TASA DE INTERES 20%',
-      fontSize: 9,
-      alignment: 'center'
-    }];
-
-    res.push(elem);
+    res.push(generateText(item));
     return res;
   }, []);
 
-  parseContent(list).map(item =>
-    docDefinition.content[0].table.body[0].push(item)
-  );
-
+  docDefinition.content[0].table.body = pipe(parseContent, generateArrayGroups)(list);
   return pdfMake.createPdf(docDefinition).open();
 };
 
